a9b561155d1eabbf51ca3bebadda7c03aad361e3
richard <richierob62@gmail.com>
2017-07-25 10:59:22 -0400

    animation added to list enter/leave

--- src/page_utils/page_list.js
+++ src/page_utils/page_list.js
@@ -6,32 +6,39 @@ import DataRows from "../page_utils/data_rows";
 import Pagination from "../page_utils/pagination";
 import { connect } from "react-redux";
 import { getMode } from "../selectors";
+import { CSSTransitionGroup } from "react-transition-group";
 
 const mstp = (state, ownProps) => ({
   mode: getMode(state[ownProps.page])
 });
-
 
 const PageListWrapper = styled.div`
     margin-bottom: 1rem;
 `;
 
 const PageList = ({ page, mode }) => {
- return mode === 'display' ?
-  <PageListWrapper>
-    <table
-      className={"table table-sm"}
-      style={{ border: "none", marginBottom: "2px" }}
+  return (
+    <CSSTransitionGroup
+      transitionName="list"
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={300}
     >
-      <thead style={{ paddingBottom: ".5rem" }}>
-        <LabelCells page={page} />
-        <FilterCells page={page} />
-      </thead>
-      <DataRows page={page} />
-    </table>
-    <Pagination page={page} />
-  </PageListWrapper>
-  : null;
-}
+      {mode === "display" &&
+        <PageListWrapper key={"the_list"}>
+          <table
+            className={"table table-sm"}
+            style={{ border: "none", marginBottom: "2px" }}
+          >
+            <thead style={{ paddingBottom: ".5rem" }}>
+              <LabelCells page={page} />
+              <FilterCells page={page} />
+            </thead>
+            <DataRows page={page} />
+          </table>
+          <Pagination page={page} />
+        </PageListWrapper>}
+    </CSSTransitionGroup>
+  );
+};
 
 export default connect(mstp)(PageList);
