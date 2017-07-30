8153577e448b2c33a23d42155e99080b7a70895c
richard <richierob62@gmail.com>
2017-07-30 16:46:53 -0400

    page change animations implemented

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { isLoggedIn } from "./selectors.js";
 import act from "./actions/";
 import asyncComponent from "./utils/async_loader";
+import { CSSTransitionGroup } from "react-transition-group";
 
 const Login = asyncComponent(() => import("./components/Login"));
 const AdTypes = asyncComponent(() => import("./components/AdTypes"));
@@ -53,190 +54,204 @@ const Main = props => {
 
   return (
     <StyledMain>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route
-          path="/ad_types"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <AdTypes
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/categories"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <Categories
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/compensation_plans"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <CompensationPlans
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/customers"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <Customers
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/fields"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <Fields
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/findinglines"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <FindingLines
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/headings"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <Headings
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/localforeign"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <LocalForeigns
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/order_lines"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <OrderLines
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/order_statuses"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <OrderStatuses
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/orders"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <Orders
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/page_types"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <PageTypes
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/payplans"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <PayPlans
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/permissions"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <Permissions
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/primary_books"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <PrimaryBooks
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/sales_reps"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <SalesReps
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/source_books"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <SourceBooks
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
-        <Route
-          path="/udacs"
-          render={({ location }) =>
-            loadPageDataOrRedirect(
-              <Udacs
-                dispatch={store.dispatch}
-                action={act.pageChange(location.pathname)}
-              />
-            )}
-        />
+      <Route
+        render={({ location }) => {
+          return (
+            <CSSTransitionGroup
+              transitionName="page"
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={300}
+            >
+              <Switch location={location} key={location.key}>
+                <Route path="/login" component={Login} />
 
-      </Switch>
+                <Route
+                  path="/ad_types"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <AdTypes
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+
+                <Route
+                  path="/categories"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <Categories
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+
+                <Route
+                  path="/compensation_plans"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <CompensationPlans
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/customers"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <Customers
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/fields"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <Fields
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/findinglines"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <FindingLines
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/headings"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <Headings
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/localforeign"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <LocalForeigns
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/order_lines"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <OrderLines
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/order_statuses"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <OrderStatuses
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/orders"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <Orders
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/page_types"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <PageTypes
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/payplans"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <PayPlans
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/permissions"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <Permissions
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/primary_books"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <PrimaryBooks
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/sales_reps"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <SalesReps
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/source_books"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <SourceBooks
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+                <Route
+                  path="/udacs"
+                  render={({ location }) =>
+                    loadPageDataOrRedirect(
+                      <Udacs
+                        dispatch={store.dispatch}
+                        action={act.pageChange(location.pathname)}
+                      />
+                    )}
+                />
+              </Switch>
+            </CSSTransitionGroup>
+          );
+        }}
+      />
     </StyledMain>
   );
 };
