63c4ea1e8dd54309d35162ea45dd6c5d59575dc8
richard <richierob62@gmail.com>
2017-09-05 17:25:27 -0400

    fix conflict with vscode chrome debugger and redux devtools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,26 +1,19 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware, { END } from 'redux-saga';
-import root_reducer from './reducers/root_reducer';
+import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware, { END } from 'redux-saga'
+import root_reducer from './reducers/root_reducer'
 
 const configureStore = () => {
+	const sagaMiddleware = createSagaMiddleware()
 
-    const sagaMiddleware = createSagaMiddleware();
+	const middlewareList = applyMiddleware(sagaMiddleware)
 
-    const middlewareList = global.running_tests
-        ? applyMiddleware(sagaMiddleware)
-        : applyMiddleware(sagaMiddleware);
+	const allMiddleWare = [ middlewareList ]
+	if (window.__REDUX_DEVTOOLS_EXTENSION__) allMiddleWare.push(window.__REDUX_DEVTOOLS_EXTENSION__())
+	const store = createStore(root_reducer, compose(...allMiddleWare))
 
-    const store = createStore(root_reducer,
-        compose(
-            middlewareList,
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    )
+	store.runSaga = sagaMiddleware.run
+	store.close = () => store.dispatch(END)
+	return store
+}
 
-    store.runSaga = sagaMiddleware.run;
-    store.close = () => store.dispatch(END);
-    return store;
-
-};
-
-export default configureStore;
+export default configureStore
